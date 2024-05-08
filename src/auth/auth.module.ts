f85bfcce2bb219c58d4f99177b4eef21b4153b25
Matheus Aguilar <matheusaguilar@MacBook-Pro-de-Matheus.local>
2024-05-08 00:19:52 -0300

    chore: change jwt key value

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { InfraModule } from 'src/infra/infra.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     InfraModule,
     PassportModule,
     JwtModule.register({
-      secret: 'abc123',
+      secret: 'DO_NOT_KEEP_THIS_VALUE_HERE',
       signOptions: { expiresIn: '1h' },
     }),
   ],
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'abc123',
+      secretOrKey: 'DO_NOT_KEEP_THIS_VALUE_HERE',
     });
   }
 
